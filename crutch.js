34b2800f3180580f8d87258512c34d0cde7886d5
Amit Vishwakarma <mail.amitvishwakarma@gmail.com>
2016-01-22 01:51:59 +0530

    Return the Promise with bining

--- crutch.js
+++ crutch.js
@@ -57,7 +57,7 @@ module.exports = function crutch(defaultOptions, callback) {
             })
             .then(function() {
                 log.info('Ready.');
-                return Promise.try(app.emit, 'ready');
+                return Promise.try(app.emit.bind(app), 'ready');
             })
             .return(app);
     });
